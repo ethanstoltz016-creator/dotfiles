7ce2ae3d46ac8b0564ab36e7708ee0ab8cbc666c
ethanstoltz016-creator <ethan.stoltz016@gmail.com>
2025-12-05 15:59:56 -0600

    working on symlink2

--- symlink2.bat
+++ symlink2.bat
@@ -1,1 +1,11 @@
+@echo off
+:: Check if running as admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+        echo Restarting as administrator...
+            powershell -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
+                exit /b
+            )
+
 powershell.exe -Command "New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\_vimrc" -Target ".\_vimrc""
+pause
